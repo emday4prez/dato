6c5931f79a48faff25f41361f0867c7996ee4e1a
Emerson <emerson.day@gmail.com>
2020-11-25 11:41:16 -0800

    added info section with svg

---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited banking with zero fees",
+  description:
+    "Get access to our exclusive app that allows you to send unlimited transatcions with no fees ever",
+  buttonLabel: "Get Started",
+  imgStart: true,
+  img: "/images/plan.svg",
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false
+};
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -17,7 +17,7 @@ const Navbar = ({ toggle }) => {
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to='/'>coinz</NavLogo>
+          <NavLogo to='/'>DATO</NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
